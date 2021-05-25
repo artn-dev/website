b64e9e8f0f2c70788920ab01b5de3186bf4def80
artn-dev <azemarteixeira@gmail.com>
2021-05-25 16:22:52 -0300

    Wrap app in bootstrap container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container-fluid min-vh-100 px-0 bg-light">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
