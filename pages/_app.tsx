6270dbc63b54a7c85a63cead644aa56d5459f248
artn-dev <azemarteixeira@gmail.com>
2021-05-25 16:25:31 -0300

    Load bootstrap bundle script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="container-fluid min-vh-100 px-0 bg-light">
       <Component {...pageProps} />
+
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossOrigin="anonymous"
+      ></script>
+
     </div>
   );
 }
