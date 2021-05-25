ac5569f9bf5fc2003e294134eddc32d3fe6e16dd
artn-dev <azemarteixeira@gmail.com>
2021-05-25 19:04:50 -0300

    Add basis of footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <>
       <Header/>
       <h1>Hello World!</h1>
+      <Footer/>
     </>
   )
 }
