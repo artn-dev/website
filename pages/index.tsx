f87c54c7a990856f7fbf2fcace4b386f7dda498a
artn-dev <azemarteixeira@gmail.com>
2021-05-25 21:46:51 -0300

    Setup cover page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import Header from '../components/Header';
-import Footer from '../components/Footer';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
     <>
-      <Header/>
-      <h1>Hello World!</h1>
-      <Footer/>
+      <div className={styles.backgroundImage}/>
+      <Header cover/>
     </>
   )
 }
