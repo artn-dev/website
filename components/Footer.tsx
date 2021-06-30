97bd388db36a74f2b611e1c186f7c24492a8d790
artn-dev <azemarteixeira@gmail.com>
2021-06-30 01:18:16 -0300

    Add custom styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,28 @@
 import Logo from './Logo'
+import styles from '../styles/Home.module.scss'
 
 const Footer = () => {
-    return (
-      <footer className="row bg-dark text-light py-5">
+  return (
+    <footer className="row bg-dark text-light py-5">
 
-          <div className="col-12 col-md-4 d-flex justify-content-center align-items-center">
-            <Logo />
+        <div className="col-12 col-md-4 d-flex justify-content-center align-items-center">
+          <Logo />
+        </div>
+
+        <div className="col-12 col-md-4 d-flex justify-content-center">
+          <div className={styles.footerLinks}>
+            <p className="m-0">Github</p>
+            <p className="m-0">Linkedin</p>
+            <p className="m-0">Website</p>
           </div>
+        </div>
 
-          <div className="col-12 col-md-4 d-flex justify-content-center">
-            <div className="w-75 d-flex align-items-center justify-content-between">
-              <p className="m-0" style={{ fontSize: 15 }}>Github</p>
-              <p className="m-0" style={{ fontSize: 15 }}>Linkedin</p>
-              <p className="m-0" style={{ fontSize: 15 }}>Website</p>
-            </div>
-          </div>
+        <div className="col">
+          <p className={styles.copyrightNotice}>© Azemar da Rosa Teixeira Neto, 2021</p>
+        </div>
 
-          <div className="col">
-            <p className="m-0 text-center" style={{ fontSize: 15 }}>© Azemar da Rosa Teixeira Neto, 2021</p>
-          </div>
-
-      </footer>
-    )
+    </footer>
+  )
 };
 
 export default Footer;
