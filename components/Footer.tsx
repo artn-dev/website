10b4c00a03263f8d95df977fbd0d66421c6ff465
artn-dev <azemarteixeira@gmail.com>
2021-06-30 01:04:39 -0300

    Fix footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,12 @@ const Footer = () => {
     return (
       <footer className="row bg-dark text-light py-5">
 
-          <div className="col">
+          <div className="col-12 col-md-4 d-flex justify-content-center align-items-center">
             <Logo />
           </div>
 
-          <div className="col">
-            <div className="d-flex align-items-center justify-content-between">
+          <div className="col-12 col-md-4 d-flex justify-content-center">
+            <div className="w-75 d-flex align-items-center justify-content-between">
               <p className="m-0" style={{ fontSize: 15 }}>Github</p>
               <p className="m-0" style={{ fontSize: 15 }}>Linkedin</p>
               <p className="m-0" style={{ fontSize: 15 }}>Website</p>
