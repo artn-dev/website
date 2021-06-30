26183815146c61ed1be9813fd5e6fbeef22faf52
artn-dev <azemarteixeira@gmail.com>
2021-06-30 02:18:00 -0300

    Update header links

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderLinkProps {
 };
 
 
-const HeaderLink = ({ children, href = "/"}: HeaderLinkProps) => (
+const HeaderLink = ({ children, href = "#"}: HeaderLinkProps) => (
     <div className="nav-item">
         <Link href={href} passHref>
             <a className="nav-link link-light" style={{
@@ -42,8 +42,8 @@ const Header = () => (
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <div className="navbar-nav ms-auto">
                         <HeaderLink>Home</HeaderLink>
+                        <HeaderLink href="#about">Sobre</HeaderLink>
                         <HeaderLink>Projetos</HeaderLink>
-                        <HeaderLink>Sobre</HeaderLink>
                     </div>
                 </div>
 
