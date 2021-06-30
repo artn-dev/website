bd8417876b380565d88ba43e5c317708cb2bd525
artn-dev <azemarteixeira@gmail.com>
2021-06-30 01:57:12 -0300

    Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import styles from '../styles/Home.module.scss';
-import classNames from 'classnames';
 import MainHero from '../components/MainHero';
 
 const Home = () => {
