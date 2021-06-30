8a3ce014841774488f4c8906de0d7214e08d66e5
artn-dev <azemarteixeira@gmail.com>
2021-06-30 01:54:05 -0300

    Refactor hero layout

--- components/MainHero.tsx
+++ components/MainHero.tsx
@@ -2,19 +2,20 @@ import styles from '../styles/Home.module.scss';
 import classNames from 'classnames';
 
 const MainHero = () => {
-    var coverClass = classNames("row", "py-5", styles.backgroundImage);
+    var coverClass = classNames("container-fluid", styles.backgroundImage);
 
     return (
         <>
             <div className={coverClass}>
-                <div className="w-50 d-flex flex-column justify-content-center" style={{
-                    paddingLeft: 100,
-                }}>
-                    <p className="fs-1 text-light mb-5">Meu nome é Azemar. Eu transformo ideias em realidade</p>
-                    <div>
-                        <button type="button" className="btn btn-primary">
-                            Conheça meu trabalho
-                        </button>
+                <div className="row h-100">
+                    <div className="col-12 col-md-6 d-flex align-items-center">
+                        <div className="container">
+                            <p className="fs-1 text-light mb-0">Meu nome é Azemar</p>
+                            <p className="fs-5 text-light mb-5">Eu transformo ideias em realidade</p>
+                            <button type="button" className="btn btn-primary">
+                                Conheça meu trabalho
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
