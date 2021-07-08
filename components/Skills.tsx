a7b5ca279681dc100f58fdcd742ebe94f82234d0
artn-dev <azemarteixeira@gmail.com>
2021-07-08 00:33:49 -0300

    Import skills from json file

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,20 +1,6 @@
-const Skills = () => {
-  const mockSkills = [
-    { name: "React.js", iconPath: "https://simpleicons.org/icons/react.svg" },
-    { name: "Next.js", iconPath: "https://simpleicons.org/icons/nextdotjs.svg" },
-    { name: "Python", iconPath: "https://simpleicons.org/icons/python.svg" },
-    { name: "Git", iconPath: "https://simpleicons.org/icons/git.svg" },
-    { name: "C++", iconPath: "https://simpleicons.org/icons/cplusplus.svg" },
-    { name: "CMake", iconPath: "https://simpleicons.org/icons/cmake.svg" },
-    { name: "OpenGL", iconPath: "https://simpleicons.org/icons/opengl.svg" },
-    { name: "Django", iconPath: "https://simpleicons.org/icons/django.svg" },
-    { name: "TypeScript", iconPath: "https://simpleicons.org/icons/typescript.svg" },
-    { name: "MySQL", iconPath: "https://simpleicons.org/icons/mysql.svg" },
-    { name: "GoogleTest", iconPath: "https://simpleicons.org/icons/google.svg" },
-    { name: "Flask", iconPath: "https://simpleicons.org/icons/flask.svg" },
-    // { name: "", iconPath: "" },
-  ]
+import skills from '../content/skills.json'
 
+const Skills = () => {
   return (
     <>
     <div id="skills" className="container px-5">
@@ -22,7 +8,7 @@ const Skills = () => {
         <p className="h2">Habilidades que tenho a oferecer</p>
       </div>
       <div className="row">
-        { mockSkills.map((skill) => (
+        { skills.map((skill) => (
           <div className="col-3 mb-4 text-center">
             <img className="mb-3" height={60} src={skill.iconPath} alt="" />
             <p className="fw-bold">{skill.name}</p>
