2663a49eb638a531cbe2677931455a621a088f03
artn-dev <artn@cin.ufpe.br>
2022-05-15 01:57:40 -0300

    Adapt layout to chakra-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,30 @@
+import { VStack, Box } from '@chakra-ui/react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MainHero from '../components/MainHero';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 
-
 const Home = () => {
   return (
-    <div className="container-fluid d-flex flex-column min-vh-100 bg-light">
-      <div className="fixed-top">
-        <Header/>
-      </div>
-
-      <div className="row">
+    <>
+      <VStack spacing={0}>
+        <Box pos='sticky' top={0} zIndex={2} w={'full'}>
+          <Header />
+        </Box>
         <MainHero />
-      </div>
-
-      <div className="row py-5">
-        <Projects />
-      </div>
-
-      <div className="row py-5">
-        <Skills />
-      </div>
-
-      <div className="row">
-        <Footer/>
-      </div>
-    </div>
-  )
-}
+        <Box py={10} w={'full'}>
+          <Projects />
+        </Box>
+        <Box py={10} w={'full'}>
+          <Skills />
+        </Box>
+        <Box w={'full'}>
+          <Footer />
+        </Box>
+      </VStack>
+    </>
+  );
+};
 
 export default Home;
