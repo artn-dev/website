6c952884550521a68c7c0f9a24991af36c6895fd
artn-dev <artn@cin.ufpe.br>
2022-05-14 21:14:11 -0300

    Add chakra-ui dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,29 @@
 import Head from 'next/head';
-import '../styles/globals.scss'
+import { ChakraProvider } from '@chakra-ui/react';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Days+One&display=swap" rel="stylesheet"/> 
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Days+One&display=swap'
+          rel='stylesheet'
+        />
       </Head>
 
-
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
 
       <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-        crossOrigin="anonymous"
+        src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'
+        integrity='sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4'
+        crossOrigin='anonymous'
       ></script>
     </>
   );
 }
 
-export default MyApp
+export default MyApp;
