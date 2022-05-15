0c2b9331a102d86d772e71a62427db077d8d5b48
artn-dev <artn@cin.ufpe.br>
2022-05-15 00:40:53 -0300

    Adapt Projects component to chakra-ui

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,30 +1,69 @@
-import projects from '../content/projects.json'
+import {
+  Box,
+  Text,
+  useBreakpointValue,
+  SimpleGrid,
+  Button,
+} from '@chakra-ui/react';
+import projects from '../content/projects.json';
 
 const Projects = () => {
-  const lastProjects = projects.slice(0, 4)
+  const lastProjects = projects.slice(0, 4);
 
   return (
     <>
-    <div id="projects" className="container px-5">
-      <div className="row mb-5">
-        <p className="h2">Veja meus projetos mais recentes</p>
-      </div>
-      <div className="row">
-          { lastProjects.map((project) => (
-              <div className="col-12 col-md-6 mb-5" key={project.title}>
-                <p className="h5 mb-3">{project.title}</p>
-                <p>{project.body}</p>
-                { project.link && (
-                  <a className="btn btn-outline-info" href={project.link} target="_blank">
-                    Saiba mais
-                  </a>
-                ) }
-              </div>
-          )) }
-        </div>
-    </div>
-    </>
-  )
-}
+      <Box w={'full'} px={'16'}>
+        <Text
+          lineHeight={1.2}
+          fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}
+          mb={'8'}
+        >
+          Veja alguns de meus projetos
+        </Text>
 
-export default Projects
+        <SimpleGrid columns={{ lg: 3, md: 2, sm: 1 }} spacing={'20px'}>
+          {lastProjects.map((project, i) => (
+            <Box
+              key={i}
+              maxW={'lg'}
+              borderWidth={'1px'}
+              borderRadius={'md'}
+              p={'5'}
+              display={'flex'}
+              flexDirection={'column'}
+            >
+              <Text fontSize={useBreakpointValue({ base: 'xl', md: '2xl' })}>
+                {project.title}
+              </Text>
+              <Text
+                color={'blackAlpha.700'}
+                fontSize={useBreakpointValue({ base: 'sm', md: 'md' })}
+                noOfLines={4}
+                mb={'2'}
+              >
+                {project.body}
+              </Text>
+              {project.link && (
+                <Box mt={'auto'}>
+                  <Button
+                    as={'a'}
+                    href={project.link}
+                    target='_blank'
+                    bg={'blue.300'}
+                    color={'white'}
+                    rounded={'full'}
+                    size={'xs'}
+                  >
+                    Saiba mais
+                  </Button>
+                </Box>
+              )}
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Box>
+    </>
+  );
+};
+
+export default Projects;
