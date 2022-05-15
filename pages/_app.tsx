50816720886f0ae3f43d1ccfc9d3f09836e77461
artn-dev <artn@cin.ufpe.br>
2022-05-15 02:12:03 -0300

    Remove bootstrap dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,11 @@
-import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
-import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Days+One&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
-
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
-
-      <script
-        src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'
-        integrity='sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4'
-        crossOrigin='anonymous'
-      ></script>
     </>
   );
 }
